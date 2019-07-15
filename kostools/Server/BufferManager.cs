48a699ee8ac6ccf6d1ad6f13f5b6f06f938d833a
cahe7cb <cahe7cb@users.noreply.github.com>
2019-07-14 23:38:26 -0300

    Add support for incremental buffer synchronization

--- kostools/Server/BufferManager.cs
+++ kostools/Server/BufferManager.cs
@@ -32,8 +32,19 @@ namespace kOS.Tools.Server
         {
             if(change.Range is null)
             {
+                // non-incremental/full buffer synchronization
                 this.Put(uri, change.Text);
                 return;
+            }
+
+            var buffer = _buffers.GetOrAdd(uri.ToString(), (key) => new StringBuilder());
+            var text = buffer.ToString();
+            int start = BufferPosition(text, change.Range.Start);
+
+            if(start != -1)
+            {
+                buffer.Remove(start, change.RangeLength);
+                buffer.Insert(start, change.Text);
             }
         }
 
@@ -42,6 +53,26 @@ namespace kOS.Tools.Server
             var scanner = new Scanner();
             var parser = new Parser(scanner);
             return parser.Parse(Get(uri), uri.ToString());
+        }
+
+        protected int BufferPosition(string text, Position position)
+        {
+            int pos = 0;
+
+            for (int i = 0; i < position.Line; i++)
+            {
+                var p = pos;
+                if ((p = text.IndexOf('\n', p)) != -1)
+                    pos = p + 1;
+            }
+
+            if (pos != -1)
+                pos += (int)position.Character;
+
+            if (!(pos < text.Length))
+                pos = -1;
+
+            return pos;
         }
     }
 }
